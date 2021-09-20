dcb5cfc3203f869f7907f12c1c73e05f3beb99da
genaromartinez <genaro.mtz1998@gmail.com>
2021-09-20 14:37:31 -0500

    feat(oficios.js): add new route and controller for oficios

---
+++ arquidiocesis-back/routes/oficios.js
@@ -0,0 +1,101 @@
+/**
+ * Module for managing 'capacitaciones'
+ * @module Oficios
+ */
+
+const add = async (firestore, req, res) => {
+  const { oficio_label } = req.body;
+
+  if (!oficio_label) {
+    return res.send({
+      error: true,
+      message: 'oficio_label is invalid',
+    });
+  }
+
+  const new_oficio_entry = {
+    value: oficio_label.toLowerCase(),
+    label: oficio_label,
+  };
+
+  // check that current role_title is not already registered
+  await firestore
+    .collection('oficios')
+    .where('label', '==', new_oficio_entry.label)
+    .get()
+    .then((snapshot) => {
+      if (!snapshot.empty) {
+        return res.send({
+          error: true,
+          message: 'Este oficio ya está creado',
+          data: snapshot,
+        });
+      }
+    });
+
+  try {
+    const collectionref = await firestore.collection('oficios');
+    const docref = await collectionref.add(new_oficio_entry); // add new role to roles collection
+    // --------- success ----------//
+    // ----------VVVVVVV-----------//
+    res.send({
+      error: false,
+      data: docref.id,
+    });
+  } catch (e) {
+    return res.send({
+      error: true,
+      message: e,
+    });
+  }
+};
+
+const getAllOficios = async (firestore, req, res) => {
+  const dataRes = {};
+  try {
+    const oficiosRef = await firestore.collection('oficios');
+    const snapshot = await oficiosRef.get();
+    snapshot.forEach((doc) => {
+      dataRes[doc.id] = doc.data();
+    });
+    res.send({
+      error: false,
+      data: dataRes,
+    });
+  } catch (e) {
+    return res.send({
+      error: true,
+      message: e,
+    });
+  }
+};
+
+const remove = async (firestore, req, res) => {
+  const { id } = req.params; //role ID
+
+  if (id == null || id === '') {
+    res.send({
+      error: true,
+      message: 'Se requiere el ID del oficio',
+    });
+  }
+
+  try {
+    await firestore.collection('oficios').doc(id).delete();
+    res.send({
+      error: false,
+      message: 'Oficio borrado exitosamente',
+    });
+  } catch (e) {
+    res.send({
+      error: true,
+      message: `Error inesperado: ${e}`,
+    });
+  }
+};
+
+module.exports = {
+  add,
+  getAllOficios,
+  remove,
+};
