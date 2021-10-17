bbee31f3f4c14c87fa0a5af2c92f600d02367b54
Enrique Abrahm García Torres <garcia.enrique.torres@gmail.com>
2021-10-17 14:40:33 -0500

    feat(estadisticas): allow to get statistics by an specific group

--- arquidiocesis-back/routes/estadisticas.js
+++ arquidiocesis-back/routes/estadisticas.js
@@ -1,33 +1,29 @@
-let FieldValue = require('firebase-admin').firestore.FieldValue;
-const moment = require('moment');
-const Util = require('./util');
-
 const getEstadisticas = async (firestore, req, res) => {
-  var servicio = {
+  const servicio = {
     publico: 0,
     privado: 0,
     ninguno: 0,
   };
 
-  var seguridadSocial = {
+  const seguridadSocial = {
     pensionado: 0,
     jubilado: 0,
     apoyo_federal: 0,
     ninguno: 0,
   };
 
-  var alergico = 0;
-  var lista_alergias = [];
-  var problema_cardiovascular = 0;
-  var problema_azucar = 0;
-  var problema_hipertension = 0;
-  var problema_sobrepeso = 0;
-  var ningun_problema = 0;
+  let alergico = 0;
+  const lista_alergias = [];
+  let problema_cardiovascular = 0;
+  let problema_azucar = 0;
+  let problema_hipertension = 0;
+  let problema_sobrepeso = 0;
+  let ningun_problema = 0;
 
-  var discapacidad = 0;
-  var lista_discapacidad = [];
+  let discapacidad = 0;
+  const lista_discapacidad = [];
 
-  var escolaridad = {
+  const escolaridad = {
     ninguno: 0,
     primaria: 0,
     secundaria: 0,
@@ -36,18 +32,113 @@ const getEstadisticas = async (firestore, req, res) => {
     profesional: 0,
   };
 
-  var total = 0;
+  let total = 0;
 
-  var miembros = await firestore.collection('miembros').get();
+  const miembrosRef = firestore.collection('miembros');
+  let miembros = null;
+
+  const zone = req.query.zone || null;
+  // Filter by zone
+  if (zone) {
+    const zonesRef = firestore.collection('zonas');
+    const zoneDoc = await zonesRef.doc(zone).get();
+
+    const decanatoRef = firestore.collection('decanatos');
+    const decanato = req.query.decanato || null;
+    const decanatoIds = [];
+    // Filter by decanato
+    if (decanato) {
+      decanatoIds.push(decanato);
+    } else {
+      const decanatoDocs = await decanatoRef
+        .where('zona', '==', zoneDoc.id)
+        .get();
+      decanatoDocs.forEach((doc) => decanatoIds.push(doc.id));
+    }
+
+    // Get Parroquias
+    const parroquiasRef = firestore.collection('parroquias');
+
+    let batches = [];
+    while (decanatoIds.length) {
+      // Firestore limits batches to 10
+      const ids = decanatoIds.splice(0, 10);
+
+      // Add request to queue
+      batches.push(
+        new Promise((response) => {
+          parroquiasRef
+            .where('decanato', 'in', ids)
+            .get()
+            .then((parroquiaDocs) => {
+              const resultIds = [];
+              parroquiaDocs.forEach((doc) => resultIds.push(doc.id));
+              response(resultIds);
+            });
+        })
+      );
+    }
+
+    // After all of the data is fetched, return it
+    const parroquiaIds = await (await Promise.all(batches)).flat();
+
+    // Get Grupos
+    const gruposRef = firestore.collection('grupos');
+
+    batches = [];
+    while (parroquiaIds.length) {
+      // Firestore limits batches to 10
+      const ids = parroquiaIds.splice(0, 10);
+
+      // Add request to queue
+      batches.push(
+        new Promise((response) => {
+          gruposRef
+            .where('parroquia', 'in', ids)
+            .get()
+            .then((grupoDocs) => {
+              const resultIds = [];
+              grupoDocs.forEach((doc) => resultIds.push(doc.id));
+              response(resultIds);
+            });
+        })
+      );
+    }
+    const grupoIds = await (await Promise.all(batches)).flat();
+
+    // Get miembros
+    batches = [];
+    while (grupoIds.length) {
+      // Firestore limits batches to 10
+      const ids = grupoIds.splice(0, 10);
+
+      // Add request to queue
+      batches.push(
+        new Promise((response) => {
+          miembrosRef
+            .where('grupo', 'in', ids)
+            .get()
+            .then((miembrosDocs) => {
+              response(miembrosDocs.docs);
+            });
+        })
+      );
+    }
+
+    miembros = await (await Promise.all(batches)).flat();
+  } else {
+    miembros = await miembrosRef.get();
+  }
+
   miembros.forEach((miembro) => {
-    var mdata = miembro.data();
+    const mdata = miembro.data();
     total++;
 
     // Checking servicio medico
-    var ficha_medica = mdata.ficha_medica;
+    const ficha_medica = mdata.ficha_medica;
     if (ficha_medica == undefined) return;
 
-    var mservicio = ficha_medica.servicio_medico;
+    const mservicio = ficha_medica.servicio_medico;
     if (mservicio == 'Público') servicio.publico++;
     else if (mservicio == 'Privado') servicio.privado++;
     else if (mservicio == 'Ninguno') servicio.ninguno++;
@@ -86,14 +177,14 @@ const getEstadisticas = async (firestore, req, res) => {
     }
 
     // Checking seguridad social
-    var mSeguridad = ficha_medica.seguridad_social;
+    const mSeguridad = ficha_medica.seguridad_social;
     if (mSeguridad == 'Ninguno') seguridadSocial.ninguno++;
     else if (mSeguridad == 'Pensionado') seguridadSocial.pensionado++;
     else if (mSeguridad == 'Jubilado') seguridadSocial.jubilado++;
     else if (mSeguridad == 'Apoyo Federal') seguridadSocial.apoyo_federal++;
 
     // Checking escolaridad
-    var mEscolaridad = mdata.escolaridad;
+    const mEscolaridad = mdata.escolaridad;
     if (mEscolaridad == 'Ninguno') escolaridad.ninguno++;
     else if (mEscolaridad == 'Primaria') escolaridad.primaria++;
     else if (mEscolaridad == 'Secundaria') escolaridad.secundaria++;
